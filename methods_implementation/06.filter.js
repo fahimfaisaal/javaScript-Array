cbb7b630a1eaf0740d2a1bf1850d25fbf2dec70f
fahim faisaal <fahimfaisal.soikot@gmail.com>
2020-12-25 07:38:48 +0600

    add implementation of filter method

---
+++ methods_implementation/06.filter.js
@@ -0,0 +1,32 @@
+//! filter is an immutable method mean it's return a new array
+console.clear()
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const evenArr = arr.filter(value => value % 2 === 0)
+
+// console.log(evenArr)
+
+//* Implementation of filter Method
+
+/**
+ * @param {Array<any>} arr 
+ * @param {function} callBack {
+ * @argument {any} arr[i]
+ * @argument {number} i
+ * @argument {Array<any>} arr
+ * }
+ * @return newArr
+ */
+function myFilter(arr, callBack) {
+    let newArr = []
+
+    for (let i = 0; i < arr.length; i++) {
+        if (callBack(arr[i], i, arr)) newArr.push(arr[i])        
+    }
+
+    return newArr
+}
+
+const odd = myFilter(arr, value => value % 2 !== 0)
+console.log(odd)
