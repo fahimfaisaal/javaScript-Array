998984f16bbbd1acb881bc452f57515af92e993c
fahim faisal <fahimfaisal.soikot@gmail.com>
2021-02-01 07:32:21 +0600

    fix some bugs

--- javaScript_Array/02.constructor vs factory.js
+++ javaScript_Array/02.constructor vs factory.js
@@ -1,20 +1,21 @@
 console.clear()
 
 //* Constructor Pattern
-const constructorArray = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) //* It's same to [] literal
+const constructorArray = new Array(10) //* for define length
 console.log(constructorArray, constructorArray.length)
 
-//* for define length
-const constructorArray2 = new Array(10)
+const constructorArray2 = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) //* It's same to [] literal
 console.log(constructorArray2, constructorArray2.length)
 
 //* Factory Pattern
-const factoryArray = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) //* it's also same to [] literal
-console.log(factoryArray)
+const factoryArray = Array(10) //* for define length
+console.log(factoryArray, factoryArray.length)
 
-const factoryArray2 = Array(10)
-console.log(factoryArray2, factoryArray2.length)
+const factoryArray2 = Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) //* it's also same to [] literal
+console.log(factoryArray2)
 
 //* Prototype are same
 console.log(constructorArray.__proto__.constructor)
-console.log(constructorArray.__proto__.constructor)
+console.log(factoryArray.__proto__.constructor)
+
+console.log(constructorArray.__proto__.constructor === factoryArray.__proto__.constructor)
