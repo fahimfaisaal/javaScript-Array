18c26fe408bcdb4b5ff4c139448140ceb2a96936
fahim faisal <fahimfaisal.soikot@gmail.com>
2021-04-13 06:05:00 +0600

    change some variable names and add semicolon

--- methods_implementation/10_shift.js
+++ methods_implementation/10_shift.js
@@ -1,9 +1,9 @@
 console.clear()
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-let fistElement = arr.shift()
-console.log('Built in method ->', fistElement, arr)
+let fistElement = arr.shift();
+console.log('Built in method ->', fistElement, arr);
 
 /**
  * @title implementation of shift method
@@ -11,18 +11,18 @@ console.log('Built in method ->', fistElement, arr)
  * @returns {any} first element of the @param arr
  */
 function myShift(arr) {
-    const shift = arr[0]
+    const firstElement = arr[0];
 
     for (
         let i = 0, len = arr.length - 1;
         i < len; i++
     ) {
-        arr[i] = arr[i + 1]
+        arr[i] = arr[i + 1];
     }
     
-    arr.length--
+    arr.length--;
 
-    return shift
+    return firstElement;
 }
 
 firstElement = myShift(arr)
